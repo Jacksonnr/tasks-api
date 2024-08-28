fcdc979e22fa6910dbf6b16808c3c3e8ad741ec8
Jackson Reis <jreis3607@gmail.com>
2024-08-28 15:41:35 -0300

    finalizando as rotas da aplicação

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -28,4 +28,73 @@ export async function tasksRoutes(app: FastifyInstance) {
 
         return listTasks 
     })
+
+    app.put('/:id', async (request, reply) => {
+        const createUpdateTaskSchema = z.object({
+            title: z.string(),
+            description: z.string(),
+        })
+
+        const getIdTask = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = getIdTask.parse(request.params)
+        const { title, description } = createUpdateTaskSchema.parse(request.body)
+
+        const result = await knex('tasks')
+            .update({ 
+                title, 
+                description,
+                updated_at : knex.fn.now()
+             })
+            .where('id', id)
+
+        if (result === 0) {
+            return reply.status(404).send('task not found!');
+        }
+
+        return reply.status(200).send('task Updated')
+    })
+    
+
+    app.delete('/:id', async (request, reply) => {
+        const getIdTask = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = getIdTask.parse(request.params)
+
+        const result = await knex('tasks')
+            .delete()
+            .where('id', id)
+
+        if (result === 0) {
+            return reply.status(404).send('task not found!');
+        }
+
+        return reply.status(200).send('task deleted')   
+    })
+
+    app.patch('/:id/complete', async(request, reply) => {
+        const getIdTask = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = getIdTask.parse(request.params)
+
+        const result = await knex ('tasks')
+        .update({
+            updated_at: knex.fn.now(),
+            completed_at :knex.fn.now()
+        })
+        .where('id', id)
+
+        if (result === 0) {
+            return reply.status(404).send('task not found!');
+        }
+
+        return reply.status(200).send('task completed')   
+    })
+    
 }
